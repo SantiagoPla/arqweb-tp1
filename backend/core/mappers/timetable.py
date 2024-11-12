d48afa2e75d64e2b5b2c94d09ec99b56102ab99c
Santiago Plá <santiago_pla@hotmail.com>
2024-11-12 00:21:22 +0000

    chore: deleted Timetible DTO

--- backend/core/mappers/timetable.py
+++ backend/core/mappers/timetable.py
@@ -5,15 +5,3 @@ def map_mongo_to_timetable_model(mongo_timetable: dict) -> Timetable:
     return Timetable(
         timetable=mongo_timetable["timetable"]
     )
-
-
-
-"""
-class Timetable(BaseModel):
-    
-    class TimeSlot(BaseModel):
-        opening_time: str  
-        closing_time: str 
-
-    timetable: Dict[str, List[TimeSlot]]
-    """
