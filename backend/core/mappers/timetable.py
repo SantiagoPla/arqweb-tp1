552fd0dc2be9674aac52b93db5f62c7fa6e5923c
Santiago Plá <santiago_pla@hotmail.com>
2024-11-12 00:23:55 +0000

    chore: deleted Timetable mapper file

--- backend/core/mappers/timetable.py
+++
@@ -1,7 +0,0 @@
-from schemas.timetable import Timetable
-
-
-def map_mongo_to_timetable_model(mongo_timetable: dict) -> Timetable:
-    return Timetable(
-        timetable=mongo_timetable["timetable"]
-    )
