8150661b431394a798bad8eaf417d4bf881b152e
Santiago Plá <santiago_pla@hotmail.com>
2024-11-13 01:59:24 +0000

    refactor: OutputOrder schema is now Order model schema

--- backend/api/user/order.py
+++ backend/api/user/order.py
@@ -1,4 +1,4 @@
-from schemas.output_order import OutputOrder
+from db.models.output_order import Order
 from schemas.input_order_creation import InputOrderCreation
 from db.repositories.restaurant import RestaurantRepository
 from core.dependencies import get_mongo_ds
@@ -11,13 +11,13 @@ router = APIRouter()
 @router.post(
     "/{restaurant_id}",
     status_code=status.HTTP_201_CREATED,
-    response_model=OutputOrder
+    response_model=Order
 )
 async def create_order(
     restaurant_id: str,
     input_order: InputOrderCreation = Body(...),
     mongo_ds=Depends(get_mongo_ds)
-) -> OutputOrder:
+) -> Order:
     
     restaurant_repository = RestaurantRepository(mongo_ds)
     restaurant_service = RestaurantService(restaurant_repository)
