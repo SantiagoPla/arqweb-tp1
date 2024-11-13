6b2747636606ab100c2ef7d86ed53b50d7a1b76e
Santiago Plá <santiago_pla@hotmail.com>
2024-11-13 03:07:30 +0000

    feat: InputOrderCreation for each case

--- backend/api/user/order.py
+++ backend/api/user/order.py
@@ -1,5 +1,5 @@
 from db.models.output_order import Order
-from schemas.input_order_creation import InputOrderCreation
+from schemas.input_order_creation import InputTableOrderCreation, InputTakeAwayOrderCreation
 from db.repositories.order import OrderRepository
 from core.dependencies import get_mongo_ds
 from services.order import OrderService
@@ -9,13 +9,29 @@ router = APIRouter()
 
 
 @router.post(
-    "/{restaurant_id}",
+    "/table-order/{restaurant_id}",
     status_code=status.HTTP_201_CREATED,
     response_model=Order
 )
-async def create_order(
+async def create_table_order(
     restaurant_id: str,
-    input_order: InputOrderCreation = Body(...),
+    input_order: InputTableOrderCreation = Body(...),
+    mongo_ds=Depends(get_mongo_ds)
+) -> Order:
+    
+    order_repository = OrderRepository(mongo_ds)
+    order_service = OrderService(order_repository)
+    return order_service.create_order(restaurant_id=restaurant_id, input_order=input_order)
+
+
+@router.post(
+    "/take-away-order/{restaurant_id}",
+    status_code=status.HTTP_201_CREATED,
+    response_model=Order
+)
+async def create_table_order(
+    restaurant_id: str,
+    input_order: InputTakeAwayOrderCreation = Body(...),
     mongo_ds=Depends(get_mongo_ds)
 ) -> Order:
     
--- backend/schemas/input_order_creation.py
+++ backend/schemas/input_order_creation.py
@@ -1,6 +1,10 @@
 from typing import Dict
 from pydantic import BaseModel, Field
 
+
 class InputOrderCreation(BaseModel):
-    table_id: int = Field(..., example=1)
     items: Dict[str, int] = Field(..., example={"Pizza": 2})
+class InputTableOrderCreation(InputOrderCreation):
+    table_id: int = Field(..., example=1)
+class InputTakeAwayOrderCreation(InputOrderCreation):
+    email: str = Field(..., example="hola@gmail.com")
