75b2825ef8553b96f3a10bf5568dfc81e32f2b41
Santiago Plá <santiago_pla@hotmail.com>
2024-11-13 01:41:05 +0000

    feat: order creation POST interface & OutputOrder schema

---
+++ backend/api/user/order.py
@@ -0,0 +1,24 @@
+from schemas.input_order_creation import InputOrderCreation
+from db.repositories.restaurant import RestaurantRepository
+from core.dependencies import get_mongo_ds
+from services.restaurant import RestaurantService
+from fastapi import APIRouter, status, Depends
+
+router = APIRouter()
+
+
+@router.post(
+    "/{restaurant_id}/{table_id}",
+    status_code=status.HTTP_201_CREATED,
+    response_model=OutputOrder
+)
+async def create_order(
+    restaurant_id: str,
+    table_id: int,
+    order: InputOrderCreation,
+    mongo_ds=Depends(get_mongo_ds)
+) -> OutputOrder:
+    
+    restaurant_repository = RestaurantRepository(mongo_ds)
+    restaurant_service = RestaurantService(restaurant_repository)
+    return restaurant_service.create_order(restaurant_id=restaurant_id, table_id=table_id, order=order)
---
+++ backend/schemas/output_order.py
@@ -0,0 +1,11 @@
+from typing import Dict
+from pydantic import BaseModel, Field
+
+class OutputOrder(BaseModel):
+    restaurant_mongo_id: str = Field(..., example="6722a6e1590f3dd285a31b03")
+    table_id: int = Field(..., example=1)
+    items: Dict[str, int] = Field(..., example={"Pizza": 2})
+    order_id: str = Field(..., example="5f4b6b3b9b3f4b3b9b3f4b3b")
+    status: str = Field(..., example="PENDING")
+    created_at: str = Field(..., example="2020-08-29T20:00:00")
+    updated_at: str = Field(..., example="2020-08-29T20:00:00")
