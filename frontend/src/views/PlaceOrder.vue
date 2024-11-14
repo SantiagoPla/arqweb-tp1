b13d322d749268b9d44a3641a7f982cbb0fb30ce
Fabrizio Prida <84802943+fabriprida@users.noreply.github.com>
2024-11-14 20:53:42 -0300

    Dev (#36)
    * Feat/menu creation business view (#19)
    
    * chore: set up BusinessMenuManagement.vue
    
    * chore: fetch restaurant into BusinessMenuManagement.vue
    
    * chore: created addMenuItemToMenu. TO DO: update BusinessMenuManagement
    
    * Feat/food orders (#20)
    
    * refactor: external is now business
    
    * refactor: separated endpoints
    
    * chore: created order.py
    
    * feat: InputOrderCreation schema created
    
    * feat: order creation POST interface & OutputOrder schema
    
    * feat: total_price added to OutputOrder schema
    
    * chore: removed Logo from /restaurant
    
    * chore: deleted restaurant mongo id from InputOrderCreation schema
    
    * refactor: OutputOrder schema is now Order model schema
    
    * feat: OrderService creation
    
    * feat: changes in Order schema
    
    * feat: InputOrderCreation for each case
    
    * feat: create order
    
    * feat: InputGetOrder schema
    
    * feat: OrderRepository.get_order
    
    * feat: InputGetOrder validation
    
    * feat: order mapper created
    
    * feat: order creation complete
    
    * feat: update restaurant API endpoints to remove 'external' prefix
    
    * Chore/update frontend requests (#21)
    
    * chore: added 'tables' field to frontend Restaurant model
    
    * chore: created InputNumberSelect
    
    * chore: updated fetchMenuById request
    
    * fix: typo on method name
    
    * feat/delete menu item (#22)
    
    * feat: menu item DELETE endpoint
    
    * feat: InputMenuItemDeletion schema
    
    * fix: parameter passed correctly in menu item deletion
    
    * Chore/load logo frontend (#23)
    
    * chore: created addLogoToRestaurant
    
    * chore: BusinessMode now sends logo to backend
    
    * chore: logo appearing in front
    
    * chore: pillow
    
    * fix: added missing Optional
    
    * chore: deleted unused pillow
    
    * feat: order items now include amount & price
    
    * feat: businessModeManagement working. TO DO: improve styles
    
    * fix: missing import
    
    ---------
    
    Co-authored-by: Fabrizio Prida <pridafabrizio@gmail.com>
    
    * Chore/business menu management (#24)
    
    * chore: finished businessMenuManagement styles
    
    * feat: products can be added and deleted from menu
    
    * chore: deleted unused constants file
    
    * chore: moved middlewares directory to core
    
    * fix: import middleware
    
    * chore: deleted unused file
    
    * fix: update client_identifier_value type to accept both string and integer
    
    * chore: deleted unused directory
    
    * Feat/order management (#25)
    
    * chore: deleted unused directory
    
    * feat: restaurant get orders
    
    * feat: update order endpoint
    
    * Feat/user menu view (#26)
    
    * chore: OverlayContent redirects to /restaurant
    
    * chore: added styles to UserMenuView
    
    * chore: PlaceTableOrder working
    
    * chore: PlaceTableOrder nows places orders correctly
    
    * style: change PlaceTableOrder name to PlaceOrder
    
    * fix: removed :tableId from PlaceOrder component (#27)
    
    * Feat/pedidos business view (#29)
    
    * chore: created BusinessOrderManagement view
    
    * chore: connected BusinessOrderManagement to BusinessMenuManagement
    
    * chore: BusinessOrderManagement fully working
    
    * chore
    
    * Chore/user order view (#30)
    
    * chore: finishes user_order_view
    
    * chore: added services
    
    * removed a (#34)
    
    * Dev 2 (#35)
    
    * commit
    
    * added initial view
    
    ---------
    
    Co-authored-by: Martin Sztajn <msztajn@colegium.com>
    
    ---------
    
    Co-authored-by: Santiago Plá <santiago_pla@hotmail.com>
    Co-authored-by: Martin Sztajn <msztajn@colegium.com>

--- frontend/src/views/PlaceOrder.vue
+++ frontend/src/views/PlaceOrder.vue
@@ -10,34 +10,29 @@
       <h2 class="title"> - Hacer pedido </h2>
     </div>
 
-    <div class="main-content">
-      <ul class="menu-list">
-        <li v-for="(item, index) in menuItems" :key="index" class="menu-item">
-          <div class="menu-item-info">
-            <strong class="menu-item-name">{{ item.name }}</strong>
-            <p class="menu-item-description">{{ item.description }}</p>
+
+
+    <div class="restaurant-info" style="margin-top: 60px">
+          <a :href="`https://web.whatsapp.com/send?phone=${restaurantPhone}&text=Hola%20${encodeURIComponent(restaurantName)}!%20Quería%20consultar%20sobre%20`" target="_blank">
+            <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" fill="green" class="bi bi-whatsapp" viewBox="0 0 16 16">
+              <path d="M13.601 2.326A7.85 7.85 0 0 0 7.994 0C3.627 0 .068 3.558.064 7.926c0 1.399.366 2.76 1.057 3.965L0 16l4.204-1.102a7.9 7.9 0 0 0 3.79.965h.004c4.368 0 7.926-3.558 7.93-7.93A7.9 7.9 0 0 0 13.6 2.326zM7.994 14.521a6.6 6.6 0 0 1-3.356-.92l-.24-.144-2.494.654.666-2.433-.156-.251a6.56 6.56 0 0 1-1.007-3.505c0-3.626 2.957-6.584 6.591-6.584a6.56 6.56 0 0 1 4.66 1.931 6.56 6.56 0 0 1 1.928 4.66c-.004 3.639-2.961 6.592-6.592 6.592m3.615-4.934c-.197-.099-1.17-.578-1.353-.646-.182-.065-.315-.099-.445.099-.133.197-.513.646-.627.775-.114.133-.232.148-.43.05-.197-.1-.836-.308-1.592-.985-.59-.525-.985-1.175-1.103-1.372-.114-.198-.011-.304.088-.403.087-.088.197-.232.296-.346.1-.114.133-.198.198-.33.065-.134.034-.248-.015-.347-.05-.099-.445-1.076-.612-1.47-.16-.389-.323-.335-.445-.34-.114-.007-.247-.007-.38-.007a.73.73 0 0 0-.529.247c-.182.198-.691.677-.691 1.654s.71 1.916.81 2.049c.098.133 1.394 2.132 3.383 2.992.47.205.84.326 1.129.418.475.152.904.129 1.246.08.38-.058 1.171-.48 1.338-.943.164-.464.164-.86.114-.943-.049-.084-.182-.133-.38-.232"/>
+            </svg>
+          </a>
+          <div>
+              <p><strong>Dirección:</strong> {{ restaurantAdress }}</p>
+              <p><strong>Teléfono:</strong> {{ restaurantPhone }}</p>
+              <p><strong>Email:</strong> {{ restaurantEmail }}</p>
+              <p><strong>Instagram:</strong> <a :href="`https://instagram.com/${restaurantInstagram}`" target="_blank">{{ restaurantInstagram }}</a></p>
+              <p><strong>Horarios:</strong> {{ restaurantOpen + ' - ' + restaurantClose }}</p>
           </div>
+           <a :href="`https://www.instagram.com/${restaurantInstagram}`" target="_blank">
+            <svg xmlns="http://www.w3.org/2000/svg"width="100" height="100" fill="violet"  class="bi bi-instagram" viewBox="0 0 16 16">
+              <path d="M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.9 3.9 0 0 0-1.417.923A3.9 3.9 0 0 0 .42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.205.526.478.972.923 1.417.444.445.89.719 1.416.923.51.198 1.09.333 1.942.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.9 3.9 0 0 0 1.416-.923c.445-.445.718-.891.923-1.417.197-.509.332-1.09.372-1.942C15.99 10.445 16 10.173 16 8s-.01-2.445-.048-3.299c-.04-.851-.175-1.433-.372-1.941a3.9 3.9 0 0 0-.923-1.417A3.9 3.9 0 0 0 13.24.42c-.51-.198-1.092-.333-1.943-.372C10.443.01 10.172 0 7.998 0zm-.717 1.442h.718c2.136 0 2.389.007 3.232.046.78.035 1.204.166 1.486.275.373.145.64.319.92.599s.453.546.598.92c.11.281.24.705.275 1.485.039.843.047 1.096.047 3.231s-.008 2.389-.047 3.232c-.035.78-.166 1.203-.275 1.485a2.5 2.5 0 0 1-.599.919c-.28.28-.546.453-.92.598-.28.11-.704.24-1.485.276-.843.038-1.096.047-3.232.047s-2.39-.009-3.233-.047c-.78-.036-1.203-.166-1.485-.276a2.5 2.5 0 0 1-.92-.598 2.5 2.5 0 0 1-.6-.92c-.109-.281-.24-.705-.275-1.485-.038-.843-.046-1.096-.046-3.233s.008-2.388.046-3.231c.036-.78.166-1.204.276-1.486.145-.373.319-.64.599-.92s.546-.453.92-.598c.282-.11.705-.24 1.485-.276.738-.034 1.024-.044 2.515-.045zm4.988 1.328a.96.96 0 1 0 0 1.92.96.96 0 0 0 0-1.92m-4.27 1.122a4.109 4.109 0 1 0 0 8.217 4.109 4.109 0 0 0 0-8.217m0 1.441a2.667 2.667 0 1 1 0 5.334 2.667 2.667 0 0 1 0-5.334"/>
+            </svg>
+          </a>
+      </div>
 
-          <span class="menu-item-price">${{ item.price }}</span>
-
-          <div class="quantity-controls">
-            <button 
-              @click="adjustQuantity(item, -1)" 
-              class="quantity-button" 
-              :disabled="getItemQuantity(item) <= 0"
-            >-</button>
-
-            <span class="quantity-display">{{ getItemQuantity(item) }}</span>
-
-            <button 
-              @click="adjustQuantity(item, 1)" 
-              class="quantity-button"
-            >+</button>
-          </div>
-        </li>
-      </ul>
-
-      <div class="order-type">
+<div class="order-type">
         <button 
           @click="chooseOrderType('delivery')" 
           class="order-button"
@@ -72,6 +67,48 @@
         />
       </div>
 
+    <div class="main-content">
+      <table class="menu-table">
+        <thead>
+          <tr>
+            <th>Plato</th>
+            <th>Descripción</th>
+            <th>Precio</th>
+            <th>Cantidad</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr 
+            v-for="(item, index) in menuItems" 
+            :key="index" 
+            class="menu-item-row" 
+            @mouseover="hover = index" 
+            @mouseleave="hover = null"
+            :class="{ 'hovered': hover === index }"
+          >
+            <td class="menu-item-name">{{ item.name }}</td>
+            <td class="menu-item-description">{{ item.description }}</td>
+            <td class="menu-item-price">${{ item.price }}</td>
+            <td class="quantity-controls">
+              <button 
+                @click="adjustQuantity(item, -1)" 
+                class="quantity-button" 
+                :disabled="getItemQuantity(item) <= 0"
+              >-</button>
+
+              <span class="quantity-display">{{ getItemQuantity(item) }}</span>
+
+              <button 
+                @click="adjustQuantity(item, 1)" 
+                class="quantity-button"
+              >+</button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+
+  
+    <p style="font-size: 20px">Total: ${{ total() }}</p>
       <button 
         @click="realizarPedido" 
         class="order-button-confirm" 
@@ -90,6 +127,13 @@
 
   const restaurantLogo = ref(null);
   const restaurantName = ref('');
+  const restaurantPhone = ref('');
+  const restaurantAdress = ref('');
+  const restaurantEmail = ref('');
+  const restaurantOpen = ref('');
+  const restaurantInstagram = ref('');
+  const restaurantClose = ref('');
+
   const route = useRoute();
   const router = useRouter();
   const restaurantId = route.params.restaurantId;
@@ -99,10 +143,27 @@
   const email = ref('');
   const tableNumber = ref('');
 
+
+ // Código actualizado sin computed
+    const total = () => {
+      if (!cart.value) return 0;
+      return Object.values(cart.value).reduce((acc, [quantity, price]) => acc + quantity * price, 0);
+    };
+
+
   onMounted(async () => {
+    
     try {
       const restaurantData = await fetchRestaurantById(restaurantId);
+      console.log('dataaa', restaurantData)
       restaurantName.value = restaurantData.name;
+      restaurantAdress.value = restaurantData.address;
+      restaurantPhone.value = restaurantData.phone_number;
+      restaurantEmail.value = restaurantData.email;
+      restaurantInstagram.value = restaurantData.instagram;
+      restaurantOpen.value = restaurantData.opening_time;
+      restaurantClose.value = restaurantData.closing_time;
+
 
       restaurantLogo.value = await fetchLogoById(restaurantId);
       menuItems.value = await fetchMenuById(restaurantId);
@@ -208,7 +269,7 @@
   }
 
   .main-content {
-    margin-top: 80px;
+    margin-top: 20px;
     padding: 20px;
     flex-grow: 1;
     display: flex;
@@ -216,75 +277,81 @@
     align-items: center;
     justify-content: flex-start;
   }
+.menu-table {
+  width: 1500px;
+  border-collapse: collapse;
+  margin-top: 20px;
+}
 
-  .menu-list {
-    list-style-type: none;
-    padding: 0;
-    width: 500px;
-    max-width: 1200px;
-  }
+.menu-table th,
+.menu-table td {
+  padding: 12px;
+  text-align: left;
+  border-bottom: 1px solid #ddd;
+}
 
-  .menu-item {
-    display: flex;
-    justify-content: space-between;
-    padding: 10px;
-    border-bottom: 1px solid #ddd;
-    width: 100%;
-    max-width: 1200px;
-    height: 80px;
-    box-sizing: border-box;
-    align-items: center;
-  }
+.menu-table th {
+  background-color: #f3f3f3;
+  color: #333;
+  font-size: 1.2rem;
+  font-weight: bold;
+}
 
-  .menu-item-info {
-    display: flex;
-    flex-direction: column;
-    align-items: flex-start;
-    width: 80%;
-    overflow: hidden;
-  }
+.menu-item-row {
+  transition: transform 0.2s, box-shadow 0.2s;
+}
 
-  .menu-item-name,
-  .menu-item-description {
-    margin: 0;
-    text-overflow: ellipsis;
-    white-space: nowrap;
-    overflow: hidden;
-  }
+.menu-item-row.hovered {
+  transform: scale(1.05);
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+  background-color: #fafafa;
+}
 
-  .menu-item-price {
-    font-size: 1.2rem;
-    color: #333;
-    flex-shrink: 0;
-    margin-right: 10px;
-  }
+.menu-item-name {
+  font-weight: bold;
+}
 
-  .quantity-button {
-    background-color: #E67E22;
-    color: white;
-    border: none;
-    padding: 5px 10px;
-    font-size: 1.2rem;
-    cursor: pointer;
-    border-radius: 5px;
-    width: 30px;
-    height: 30px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
+.menu-item-description {
+  color: #666;
+  font-size: 0.9rem;
+}
 
-  .quantity-controls {
-    display: flex;
-    align-items: center;
-  }
+.menu-item-price {
+  font-size: 1.1rem;
+  font-weight: bold;
+  color: #333;
+}
 
-  .quantity-display {
-    margin: 0 10px;
-    font-size: 1.2rem;
-    color: #333;
-  }
+.quantity-controls {
+  display: flex;
+  align-items: center;
+}
 
+.quantity-button {
+  background-color: #E67E22;
+  color: white;
+  border: none;
+  padding: 5px 10px;
+  font-size: 1rem;
+  cursor: pointer;
+  border-radius: 5px;
+  width: 30px;
+  height: 30px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.quantity-button:disabled {
+  background-color: #ccc;
+  cursor: not-allowed;
+}
+
+.quantity-display {
+  margin: 0 10px;
+  font-size: 1rem;
+  color: #333;
+}
   .order-button-confirm {
     margin-top: 20px;
     background-color: #28a745;
@@ -304,6 +371,7 @@
   }
 
   .order-type {
+    margin-top: 50px;
   display: flex;
   justify-content: space-around;
   width: 100%;
@@ -318,7 +386,6 @@
     font-size: 1.2rem;  /* Tamaño de fuente más pequeño */
     cursor: pointer;
     border-radius: 5px;
-    width: 150px;  /* Ajuste de tamaño */
   }
 
   .order-button:disabled {
@@ -333,5 +400,35 @@
     width: 100%;
     max-width: 500px;
 }
+
+.restaurant-info {
+  margin-top: 60px;display: flex;width: 1400px;justify-content: space-between;
+    margin-top: 20px;
+    padding: 20px;
+    background-color: #f9f9f9;
+    border-radius: 5px;
+    max-width: 100%;
+    text-align: left;
+  }
+
+  .restaurant-info h3 {
+    font-size: 1.8rem;
+    font-weight: bold;
+    margin-bottom: 10px;
+  }
+
+  .restaurant-info p {
+    font-size: 1rem;
+    margin: 5px 0;
+  }
+
+  .restaurant-info a {
+    color: #3498db;
+    text-decoration: none;
+  }
+
+  .restaurant-info a:hover {
+    text-decoration: underline;
+  }
 
 </style>
--- frontend/src/views/UserMode.vue
+++ frontend/src/views/UserMode.vue
@@ -3,11 +3,85 @@
     <h1 class="title">Mapa de Restaurantes</h1>
     <MapComponent class="map" />
   </div>
+
+   <div class="cards" style="margin-top: 50px;display: flex;justify-content: space-around;flex-wrap: wrap;">
+    <div v-for="(restaurant, index) in visibleRestaurants" :key="restaurant.name" :class="['card', isOpen(restaurant) ? 'open' : 'closed']">
+      <img 
+        :src="restaurant.hasLogo ? restaurant.logo : 'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/16/a4/d7/f1/salon.jpg'" 
+        alt="Imagen del restaurante" 
+        class="card-image" 
+      />
+      <div class="card-content">
+        <h2 class="card-title">{{ restaurant.name }}</h2>
+        <p><strong>Dirección:</strong> {{ restaurant.address }}</p>
+        <p style="margin-bottom: 15px"><strong>Horario:</strong> {{ restaurant.opening_time }} - {{ restaurant.closing_time }}</p>
+       <div style="display: flex; justify-content: space-evenly;">
+          <a :href="`https://web.whatsapp.com/send?phone=${restaurant.phone_number}&text=Hola%20${encodeURIComponent(restaurant.name)}!%20Quería%20consultar%20sobre%20`" target="_blank">
+            <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="black" class="bi bi-whatsapp" viewBox="0 0 16 16">
+              <path d="M13.601 2.326A7.85 7.85 0 0 0 7.994 0C3.627 0 .068 3.558.064 7.926c0 1.399.366 2.76 1.057 3.965L0 16l4.204-1.102a7.9 7.9 0 0 0 3.79.965h.004c4.368 0 7.926-3.558 7.93-7.93A7.9 7.9 0 0 0 13.6 2.326zM7.994 14.521a6.6 6.6 0 0 1-3.356-.92l-.24-.144-2.494.654.666-2.433-.156-.251a6.56 6.56 0 0 1-1.007-3.505c0-3.626 2.957-6.584 6.591-6.584a6.56 6.56 0 0 1 4.66 1.931 6.56 6.56 0 0 1 1.928 4.66c-.004 3.639-2.961 6.592-6.592 6.592m3.615-4.934c-.197-.099-1.17-.578-1.353-.646-.182-.065-.315-.099-.445.099-.133.197-.513.646-.627.775-.114.133-.232.148-.43.05-.197-.1-.836-.308-1.592-.985-.59-.525-.985-1.175-1.103-1.372-.114-.198-.011-.304.088-.403.087-.088.197-.232.296-.346.1-.114.133-.198.198-.33.065-.134.034-.248-.015-.347-.05-.099-.445-1.076-.612-1.47-.16-.389-.323-.335-.445-.34-.114-.007-.247-.007-.38-.007a.73.73 0 0 0-.529.247c-.182.198-.691.677-.691 1.654s.71 1.916.81 2.049c.098.133 1.394 2.132 3.383 2.992.47.205.84.326 1.129.418.475.152.904.129 1.246.08.38-.058 1.171-.48 1.338-.943.164-.464.164-.86.114-.943-.049-.084-.182-.133-.38-.232"/>
+            </svg>
+          </a>
+           <a :href="`https://www.instagram.com/${restaurant.instagram}`" target="_blank">
+            <svg xmlns="http://www.w3.org/2000/svg"width="26" height="26" fill="black"  class="bi bi-instagram" viewBox="0 0 16 16">
+              <path d="M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.9 3.9 0 0 0-1.417.923A3.9 3.9 0 0 0 .42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.205.526.478.972.923 1.417.444.445.89.719 1.416.923.51.198 1.09.333 1.942.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.9 3.9 0 0 0 1.416-.923c.445-.445.718-.891.923-1.417.197-.509.332-1.09.372-1.942C15.99 10.445 16 10.173 16 8s-.01-2.445-.048-3.299c-.04-.851-.175-1.433-.372-1.941a3.9 3.9 0 0 0-.923-1.417A3.9 3.9 0 0 0 13.24.42c-.51-.198-1.092-.333-1.943-.372C10.443.01 10.172 0 7.998 0zm-.717 1.442h.718c2.136 0 2.389.007 3.232.046.78.035 1.204.166 1.486.275.373.145.64.319.92.599s.453.546.598.92c.11.281.24.705.275 1.485.039.843.047 1.096.047 3.231s-.008 2.389-.047 3.232c-.035.78-.166 1.203-.275 1.485a2.5 2.5 0 0 1-.599.919c-.28.28-.546.453-.92.598-.28.11-.704.24-1.485.276-.843.038-1.096.047-3.232.047s-2.39-.009-3.233-.047c-.78-.036-1.203-.166-1.485-.276a2.5 2.5 0 0 1-.92-.598 2.5 2.5 0 0 1-.6-.92c-.109-.281-.24-.705-.275-1.485-.038-.843-.046-1.096-.046-3.233s.008-2.388.046-3.231c.036-.78.166-1.204.276-1.486.145-.373.319-.64.599-.92s.546-.453.92-.598c.282-.11.705-.24 1.485-.276.738-.034 1.024-.044 2.515-.045zm4.988 1.328a.96.96 0 1 0 0 1.92.96.96 0 0 0 0-1.92m-4.27 1.122a4.109 4.109 0 1 0 0 8.217 4.109 4.109 0 0 0 0-8.217m0 1.441a2.667 2.667 0 1 1 0 5.334 2.667 2.667 0 0 1 0-5.334"/>
+            </svg>
+          </a>
+        </div>        
+      </div>
+    </div>
+  </div>
+
+  <button @click="toggleShowMore" style="margin-top: 20px" class="show-more-btn">
+    {{ showMore ? 'Mostrar menos' : 'Mostrar más' }}
+</button>
+
 </template>
 
 <script setup>
 import MapComponent from '../components/MapComponent.vue';
+import { onMounted, ref, computed } from 'vue';
 
+import { fetchRestaurants } from '../services/restaurantService'; // Importa el servicio
+const restaurantData = ref([]);
+const showMore = ref(false);
+const visibleRestaurants = computed(() => {
+  return showMore.value ? restaurantData.value : restaurantData.value.slice(0, 4);
+});
+
+console.log('verrrr', restaurantData)
+const isOpen = (restaurant) => {
+  const currentTime = new Date();
+  const openingTime = new Date();
+  const closingTime = new Date();
+
+  const [openingHour, openingMinute] = restaurant.opening_time.split(':');
+  const [closingHour, closingMinute] = restaurant.closing_time.split(':');
+
+  openingTime.setHours(openingHour, openingMinute, 0, 0);
+  closingTime.setHours(closingHour, closingMinute, 0, 0);
+
+  const currentHour = currentTime.getHours();
+  const currentMinute = currentTime.getMinutes();
+
+  const openingTotalMinutes = parseInt(openingHour) * 60 + parseInt(openingMinute);
+  const closingTotalMinutes = parseInt(closingHour) * 60 + parseInt(closingMinute);
+  const currentTotalMinutes = currentHour * 60 + currentMinute;
+
+  if (closingTotalMinutes > openingTotalMinutes) {
+    return currentTotalMinutes >= openingTotalMinutes && currentTotalMinutes <= closingTotalMinutes;
+  } else {
+    // Caso en que el cierre es después de la medianoche
+    return currentTotalMinutes >= openingTotalMinutes || currentTotalMinutes <= closingTotalMinutes;
+  }
+};
+
+const toggleShowMore = () => {
+  showMore.value = !showMore.value;
+};
+
+onMounted(async () => {
+  restaurantData.value = await fetchRestaurants();
+});
 </script>
 
 <style>
@@ -27,4 +101,106 @@ import MapComponent from '../components/MapComponent.vue';
   font-family: 'Poppins', sans-serif;
   color: #2C3E50;
 }
+
+.card {
+  width: 250px;
+  margin: 5px;
+  border-radius: 10px;
+  overflow: hidden;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  transition: transform 0.3s, box-shadow 0.3s;
+}
+
+.card:hover {
+  transform: scale(1.05);
+  box-shadow: 0 8px 16px rgba(0, 0, 0, 0.2);
+}
+
+.card.open {
+  background-color: #e0ffe0;
+  border: 2px solid black;
+}
+
+.card.closed {
+  background-color: #ffe0e0;
+  border: 2px solid #f44336;
+  opacity: 0.6;
+}
+
+.card-image {
+  width: 100%;
+  height: 150px;
+  object-fit: cover;
+}
+
+.card-content {
+  padding: 20px;
+}
+
+.card-title {
+  font-size: 1.5em;
+  margin-bottom: 10px;
+}
+
+.card.open .card-title {
+  color: black;
+}
+
+.card.closed .card-title {
+  color: #f44336;
+}
+
+.card p {
+  margin: 5px 0;
+}
+
+.card.open p {
+  color: #2e7d32;
+}
+
+.card.closed p {
+  color: #c62828;
+}
+
+.dynamic-title {
+  font-size: 2.5em;
+  color: white; /* Tomate */
+  text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.3);
+}
+
+.show-more-btn {
+  background-color: black;
+  color: white;
+  font-family: 'Arial', sans-serif;
+  font-size: 1.2em;
+  padding: 10px 20px;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  transition: background-color 0.3s, transform 0.3s;
+}
+
+.show-more-btn:hover {
+  background-color: #333;
+  transform: scale(1.05);
+}
+
+@keyframes fadeIn {
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
+}
+
+@keyframes colorChange {
+  from {
+    color: #ff6347; /* Tomate */
+  }
+  to {
+    color: #4682b4; /* Azul acero */
+  }
+}
+
 </style>
